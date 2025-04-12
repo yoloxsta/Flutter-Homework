b3ca32eb6cd581e493874b5e3484005338b6827a
Soe Tint Aung <soetintaunghz10@gmail.com>
2025-04-12 22:37:55 +0630

    lab2

---
+++ my_dart_example/bin/lab2.dart
@@ -0,0 +1,29 @@
+void main() {
+  // String - text
+  String name = 'Soe Tintaung';
+  print('Name: $name');
+
+  // int - whole numbers
+  int age = 30;
+  print('Age: $age');
+
+  // double - decimal numbers
+  double height = 5.9;
+  print('Height: $height');
+
+  // bool - true or false
+  bool isDev = true;
+  print('Is Developer: $isDev');
+
+  // var - automatically infers the type
+  var city = 'Yangon';
+  print('City: $city');
+
+  // final - can’t be changed (runtime constant)
+  final country = 'Myanmar';
+  print('Country: $country');
+
+  // const - compile-time constant
+  const pi = 3.1416;
+  print('PI: $pi');
+}
--- my_dart_example/bin/my_dart_example.dart
+++ my_dart_example/bin/my_dart_example.dart
@@ -1,5 +1,9 @@
-import 'package:my_dart_example/my_dart_example.dart' as my_dart_example;
+// import 'package:my_dart_example/my_dart_example.dart' as my_dart_example;
 
-void main(List<String> arguments) {
-  print('Hello world: ${my_dart_example.calculate()}!');
+// void main(List<String> arguments) {
+//   print('Hello world: ${my_dart_example.calculate()}!');
+// }
+
+void main() {
+  print("Hello, Dart! Let's learn step-by-step!");
 }
