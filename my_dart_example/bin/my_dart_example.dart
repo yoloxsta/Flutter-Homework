965d2978c3825130022eaaf993287be023cc2ad6
Soe Tint Aung <soetintaunghz10@gmail.com>
2025-04-12 21:35:52 +0630

    test

---
+++ my_dart_example/bin/my_dart_example.dart
@@ -0,0 +1,5 @@
+import 'package:my_dart_example/my_dart_example.dart' as my_dart_example;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_dart_example.calculate()}!');
+}
---
+++ my_dart_example/test/my_dart_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:my_dart_example/my_dart_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
